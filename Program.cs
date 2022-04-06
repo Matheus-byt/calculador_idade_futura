8c37f44eac305c8b1099916b5d6ebfd97456485d
Matheus Augusto Lopes de Macedo <84509657+Matheus-byt@users.noreply.github.com>
2022-04-06 16:07:21 -0300

    Initial
    Initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Testing
+{
+    class Cadastro
+    {
+        static void Main()
+        {
+
+            // Calculador de idade e idade futura 2025
+
+            int ano, atual, idade, idadefutura;
+
+            Console.WriteLine("Digite o seu ano de nascimento: ");
+            ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano atual: ");
+            atual = int.Parse(Console.ReadLine());
+
+            idade = (atual - ano); 
+
+            idadefutura = (idade + 3);
+
+            Console.WriteLine($"Você hoje tem {idade} anos de idade, e em 2025 você vai ter: {idadefutura} anos de idade. ");
+
+
+
+            
+
+        }
+    }
+}
